27caf8f4d75abbd7ec908ec02ee9f25848d08811
jeffreyadamo <58490053+jeffreyadamo@users.noreply.github.com>
2020-04-11 11:47:33 -0700

    working on constraints

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 //Test our API
-function APIcall(keyword){
-    var APIKey = "74d82ee79a804056882eece5c8be4141";
-    // var keyword = '';
-    var numberOfResults = 5;
-   
-    //Go to https://spoonacular.com/food-api/docs#Search-Recipes-Complex to see search criteria
-        //"&addRecipeInformation=true" includes a lot more info
-        //"&fillIngredients=true" Add info about the used and missing ingredients in each recipe
+function APIcall(keyword) {
+  var APIKey = "74d82ee79a804056882eece5c8be4141";
+  // var keyword = '';
+  var numberOfResults = 5;
 
-    var queryURL =
-    "https://api.spoonacular.com/recipes/complexSearch?query="+ keyword +"&number="+ numberOfResults + "&addRecipeInformation=true&fillIngredients=true&apiKey=" + APIKey;
+  //Go to https://spoonacular.com/food-api/docs#Search-Recipes-Complex to see search criteria
+  //"&addRecipeInformation=true" includes a lot more info
+  //"&fillIngredients=true" Add info about the used and missing ingredients in each recipe
+  //&diet= + diet +
+  var diet = "diary free"
 
-    //Setup AJAX call to Spoonacular API:
-    $.ajax({
-    url:queryURL,
+  var queryURL =
+    "https://api.spoonacular.com/recipes/complexSearch?query=" +
+    keyword +
+    "&number=" + numberOfResults +
+    "&addRecipeInformation=true&fillIngredients=true&diet=" + diet + "&apiKey=" +
+    APIKey;
+
+  //Setup AJAX call to Spoonacular API:
+  $.ajax({
+    url: queryURL,
     method: "GET",
-    })
-    .then(function (response) {
+  }).then(function (response) {
     console.log(response);
-    });
+
+    ////////////////////////////////
+    //Setup diets: when "radio" boxes under searched are checked, only these vegetarian, vega, glutenFree, 
+
+    //vegetarian
+    var veganFree = response.results[0].vegetarian;
+
+    //vegan
+    var veganFree = response.results[0].vegan;
+
+    //gluten free
+    var glutanFree = response.results[0].glutanFree;
+  });
 }
 
-APIcall("chicken");
+APIcall("cookies");
