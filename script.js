c7979a007331d041281f0e85f2e63cefcedaacd1
alyssahellrung <alyssahellrung@gmail.com>
2020-04-11 13:01:13 -0700

    response calls for results and some html changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Test our API
 function APIcall(keyword){
-    var APIKey = "74d82ee79a804056882eece5c8be4141";
+    var APIKey = "454a86eaeacb46388aa9439b5c0c474e";
     // var keyword = '';
     var numberOfResults = 5;
    
@@ -17,24 +17,37 @@ function APIcall(keyword){
     method: "GET",
     })
     .then(function (response) {
-
+        console.log(response);
+        
+        //Puts the title on the recipe card
         var recipeName = response.results[0].title;
-        console.log(recipeName);
+        $("#name1").append(recipeName);
         
+        //Adds image to the recipe card     
         var img = response.results[0].image;
-        var recipeImg = $("<img src=" + img + ">");
-        $("#photo").append(recipeImg);
+        var recipeImg = $("#image1").attr("src", img);
+        $("#image1").append(recipeImg);
         
+        //Adds ingredients to the recipe card
+        var ingredientsDiv = $("#ingredients1").text("Ingredients: ");
+          for (i = 0; i < response.results[0].missedIngredients.length; i++) {
+          var ingredients = response.results[0].missedIngredients[i].originalString;
+          ingredientsDiv.append(ingredients);
+        }
 
-        var instructions = response.results[0].analyzedInstructions;
-        console.log(instructions);
+        //Adds instructions to the recipe card
+        var instructionsDiv = $("#instructions1").text("Instructions: ");
+          for (j = 0; j < response.results[0].analyzedInstructions[0].steps.length; j++) {
+          var instructions = response.results[0].analyzedInstructions[0].steps[j].step;
+          instructionsDiv.append(instructions);
+        }
         
+        //Adds the source url to the recipe card
         var source = response.results[0].sourceUrl;
         console.log(source);
-        
-
-    console.log(response);
-    });
+        var a1 = $("<a>").attr("href", source).text(source);
+        $("#source1").append(a1);
+ });
 }
 
 APIcall("chicken");
