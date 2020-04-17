a6fb00327ed7dc085e376e32b7acd8dfe809f702
alyssahellrung <alyssahellrung@gmail.com>
2020-04-16 19:03:36 -0700

    Changed source html display

--- script.js
+++ script.js
@@ -5,94 +5,6 @@ $(".diet-choice").on("click", function() {
   console.log("diet is now " + diet);
 })
 
-// APIcall("tomatoes");
-//Test our API
-function APIcall(keyword){
-  var APIKey = "74d82ee79a804056882eece5c8be4141";
-
-  //API query URL search criteria:
-  //// Go to https://spoonacular.com/food-api/docs#Search-Recipes-Complex to see search criteria
-  var numberOfResults = 4;
-  var number = "&number=" + numberOfResults;
-  var addRecipeInformation = "&addRecipeInformation=true";
-  var fillIngredients = "&fillIngredients=true";
-  var diet = ""
-  var dietChoices = "&diet=" + diet;
-  var API = "&apiKey=" + APIKey;
-
-    //"&addRecipeInformation=true" includes a lot more info
-    //"&fillIngredients=true" Add info about the used and missing ingredients in each recipe
-    //"&diet=''"" allows to toggle from vegan, vegetarian, glutenFree
-
-  var queryURL = 
-  "https://api.spoonacular.com/recipes/complexSearch?query=" 
-    + keyword 
-    + number
-    + addRecipeInformation
-    + fillIngredients
-    + dietChoices
-    + API;
-
-    console.log("queryURL is " + queryURL);
-
-  //Setup AJAX call to Spoonacular API:
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    })
-    .then(function (response) {
-        console.log(response);
-
-        for (x = 0; x < 4; x++) {
-          var cardDiv = $("<div class='medium-6 columns'>");
-          var nameEl = $("<h4 id='name'>");
-          var imgEl = $("<img class='feature-image'>");
-          var ingredientsDiv = $("<div class='feature-ingredients' id='ingredients'>");
-          var instructionsDiv = $("<div class='feature-recipe'>");
-          var sourceEl = $("<div>");
-        
-        //Puts the title on the recipe card
-        var recipeName = response.results[x].title;
-        nameEl.append(recipeName);
-        cardDiv.append(nameEl);
-                    
-        //Adds image to the recipe card     
-        var img = response.results[x].image;
-        imgEl.attr("src", img);
-        cardDiv.append(imgEl);
-           
-        //Adds ingredients to the recipe card
-        // var ingredientsDiv = $("#ingredients").text("Ingredients: ");
-        var ulIngredients = $("<ul>");
-        
-          for (i = 0; i < response.results[x].missedIngredients.length; i++) {
-          var ingredients = response.results[x].missedIngredients[i].originalString;   
-          ulIngredients.append($("<li>").append(ingredients));
-          }
-          ingredientsDiv.text("Ingredients: ");
-          ingredientsDiv.append(ulIngredients);
-          cardDiv.append(ingredientsDiv);
-
-        //Adds instructions to the recipe card
-        var olInstructions = $("<ol>");
-          for (j = 0; j < response.results[x].analyzedInstructions[0].steps.length; j++) {
-          var instructions = response.results[x].analyzedInstructions[0].steps[j].step;
-          olInstructions.append($("<li>").append(instructions));
-        }
-        instructionsDiv.text("Instructions: ");
-        instructionsDiv.append(olInstructions);
-        cardDiv.append(instructionsDiv);
-        
-        //Adds the source url to the recipe card
-        var source = response.results[x].sourceUrl;
-        var a1 = $("<a>").attr("href", source).text("Source");
-        sourceEl.append(a1);
-        cardDiv.append(sourceEl);
-        $("#recipeCard").append(cardDiv);
-
-      }
- });
-}
 
 ////////////////////////////////////////////////////////////
 
@@ -193,7 +105,7 @@ function fetchRecipes(keyword, numberOfResults, intolerances) {
         
         //Adds the source url to the recipe card
         var source = response.results[x].sourceUrl;
-        var a1 = $("<a>").attr("href", source).text("Source");
+        var a1 = $("<a>").attr("href", source).text(source);
         sourceEl.append(a1);
         cardDiv.append(sourceEl);
         $("#recipeCard").append(cardDiv);
